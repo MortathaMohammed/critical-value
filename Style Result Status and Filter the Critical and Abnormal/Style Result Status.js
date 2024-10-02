875c7c16b69b1dd18aee24e637d90e4c93ed5635
MortathaMohammed <mortathamohammedalyasiry@gmail.com>
2024-10-02 03:41:54 +0300

    add two files show filter the the list in abnormal and crirical and style the result status

---
+++ Style Result Status and Filter the Critical and Abnormal/Style Result Status.js
@@ -0,0 +1,34 @@
+frappe.listview_settings['Lab Test'] = {
+    onload: function (listview) {
+        // Button for Critical Results (Red Button)
+        const critical_button = listview.page.add_button('Critical', function () {
+            frappe.route_options = {
+                "result_status": ["=", "Critical"]
+            };
+            frappe.set_route('List', 'Lab Test');
+        });
+        $(critical_button).css({
+            "background-color": "#dc3545", // Red
+            "color": "white",
+            "border": "none",
+            "padding": "5px 10px",
+            "border-radius": "4px"
+        });
+
+        // Button for Abnormal Results (Yellow Button)
+        const abnormal_button = listview.page.add_button('Abnormal', function () {
+            frappe.route_options = {
+                "result_status": ["=", "Abnormal"]
+            };
+            frappe.set_route('List', 'Lab Test');
+        });
+        $(abnormal_button).css({
+            "background-color": "#ffc107", // Yellow
+            "color": "black",
+            "border": "none",
+            "padding": "5px 10px",
+            "border-radius": "4px"
+        });
+
+    }
+};
